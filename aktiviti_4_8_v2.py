e235902237ed9db03da6f7432401a73d36ab839f
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-09 11:00:35 +0800

    Update aktiviti_4_8_v2.py 1

--- aktiviti_4_8_v2.py
+++ aktiviti_4_8_v2.py
@@ -2,28 +2,31 @@ jenis_kek = ['keju','mentega','pelangi','kopi']
 harga_kek = [40, 35, 35, 30]
 jumlah = [0,0,0,0]
 
-def get_tempahan():
-    a = int(input("Masukkan tempahan untuk kek keju: "))
+..... get_tempahan():
+    a = .....(input("Masukkan tempahan untuk kek keju: ")) #pengguna masukkan nombor integer untuk tempahan kek
     b = int(input("Masukkan tempahan untuk kek mentega: "))
     c = int(input("Masukkan tempahan untuk kek pelangi: "))
-    d = int(input("Masukkan tempahan untuk kek kopi: "))
-    tempahan = [a, b, c, d]
-    return tempahan
+    d = int(input("Masukkan tempahan untuk kek kopi: ").....
+    tempahan = [a, b, c, .....]
+    return ............
     
-def jumlah_harga(tempahan): # user-defined function
-    for i in range(4): # struktur kawalan ulangan "for"
-        jumlah[i] = harga_kek[i] * tempahan[i]
-    return jumlah # keyword: return
+def jumlah_harga(.........): # user-defined function ini menggunakan parameter "tempahan"
+    for i in range(.....): # struktur kawalan ulangan "for" sebanyak 4 kali
+        jumlah[i] = harga_kek[.....] * tempahan[.....]
+    return ....... # memulangkan senarai "jumlah"
 
 def cetak():
     tempah = get_tempahan()
-    jum = jumlah_harga(tempah)
-    print("\n\nTempahan anda ialah: ")
-    print(f"{tempah[0]} kek {jenis_kek[0]} : {jum[0]}")
-    print(f"{tempah[1]} kek {jenis_kek[1]} : {jum[1]}")
+    jum = ..................(tempah) # jum diumpukkan kepada function "jumlah_harga" dengan argumen "tempah"
+    print("\nTempahan anda ialah: ")
+    print(f"{tempah[.....]} kek {jenis_kek[0]} : {jum[0]}")
+    print(f"{tempah[1]} kek {jenis_kek[.....]} : {jum[1]}")
     print(f"{tempah[2]} kek {jenis_kek[2]} : {jum[2]}")
-    print(f"{tempah[3]} kek {jenis_kek[3]} : {jum[3]}")
-    print(f"\nJumlah harga untuk tempahan ialah RM {sum(jum)}")
+    print(f"{tempah[3]} kek {jenis_kek[3]} : {jum[.....]}")
+    print(f"\nJumlah harga untuk tempahan ialah RM {.....(jum)}")
 
+#############################
+# Jangan ubah kod di bawah!
+#############################
 if __name__ == "__main__":
     cetak()
